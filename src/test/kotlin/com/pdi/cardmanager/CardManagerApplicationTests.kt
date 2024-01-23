5aad52fa5e22b8ac8894cebbf76798bb7eeed248
Daniel Correa Alves <dancorrea@pagseguro.com>
2024-01-23 15:55:50 -0300

    first commit

---
+++ src/test/kotlin/com/pdi/cardmanager/CardManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pdi.cardmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CardManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
