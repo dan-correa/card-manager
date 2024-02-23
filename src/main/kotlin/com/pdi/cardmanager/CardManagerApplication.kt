bf795ff19aad5c989bb085d976e985b1fcaa2a95
Daniel Correa Alves <dancorrea@pagseguro.com>
2024-02-23 10:10:20 -0300

    #Setup *ajustando o migration *ajuste no build.gradle *criação de script para subir banco direto do wrapper

--- src/main/kotlin/com/pdi/cardmanager/CardManagerApplication.kt
+++ src/main/kotlin/com/pdi/cardmanager/CardManagerApplication.kt
@@ -2,8 +2,10 @@ package com.pdi.cardmanager
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
+@EnableTransactionManagement
 class CardManagerApplication
 
 fun main(args: Array<String>) {
